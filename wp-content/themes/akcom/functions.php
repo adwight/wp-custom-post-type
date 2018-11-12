ce7e6eaaa63fe8919bce5f68387a779d8c7da54b
AJ <alainkassabian@gmail.com>
2018-11-12 09:17:45 -0500

    Added custom fields and action hook for post type

--- wp-content/themes/akcom/functions.php
+++ wp-content/themes/akcom/functions.php
@@ -18,18 +18,7 @@ function create_post_types() {
     );
 }
 
-function create_post_type() {
-  register_post_type( 'acme_product',
-    array(
-      'labels' => array(
-        'name' => __( 'Products' ),
-        'singular_name' => __( 'Product' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-    )
-  );
-}
+add_action( 'init', 'create_post_types' );
 
 function alaink_scripts()
 {
